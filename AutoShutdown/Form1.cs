313ac216b791f3e10e404102b46f621215303d70
George Wells <padeoe@gmail.com>
2015-07-31 18:59:54 +0800

    init

---
+++ AutoShutdown/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoShutdown
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+		private void textBox1_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Enter) {
+				Program.excuteShutdown(this.textBox1.Text);
+			}
+				
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Program.cancelShutdown();
+		}
+	}
+}
---
+++ AutoShutdown/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoShutdown
+{
+	static class Program
+	{
+		/// <summary>
+		/// 应用程序的主入口点。
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+
+		}
+		static String executeCommand(String argument)
+		{
+			// Start the child process.
+			Process p = new Process();
+			// Redirect the output stream of the child process.
+			p.StartInfo.UseShellExecute = false; p.StartInfo.CreateNoWindow = true;
+			p.StartInfo.RedirectStandardOutput = true;
+			p.StartInfo.FileName = "shutdown.exe";
+			p.StartInfo.Arguments = argument;
+			p.Start();
+			String output = null;
+			while (true)
+			{
+				String tmp = p.StandardOutput.ReadToEnd();
+				if (tmp == "")
+					break;
+				output += tmp;
+			}
+			//p.WaitForExit();
+			return output;
+		}
+		public static void excuteShutdown(string text)
+		{
+			int minute;
+			if (Int32.TryParse(text, out minute))
+			{
+				executeCommand("-s -t " + minute * 60);
+			}
+		}
+		public static void cancelShutdown() {
+			executeCommand("-a");
+		}
+    }
+}
